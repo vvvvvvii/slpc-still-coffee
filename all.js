10a40145b19c3e3fdf43c5a4c25f20eed328dac6
圈圈 <vvvvvvii@yahoo.com.tw>
2023-01-11 00:04:32 +0800

    layout ok rwd ok

--- all.js
+++ all.js
@@ -1,18 +1,36 @@
-const lgNum = document.querySelector("#lgNum");
-const mdNum = document.querySelector("#mdNum");
-const smNum = document.querySelector("#smNum");
+const yirgacheffeLgNum = document.querySelector("#yirgacheffeLgNum");
+const yirgacheffeMdNum = document.querySelector("#yirgacheffeMdNum");
+const yirgacheffeSmNum = document.querySelector("#yirgacheffeSmNum");
 
-function handleNum(size, action) {
+const costaricaLgNum = document.querySelector("#costaricaLgNum");
+const costaricaMdNum = document.querySelector("#costaricaMdNum");
+const costaricaSmNum = document.querySelector("#costaricaSmNum");
+
+const minusBtns = document.querySelectorAll(".minus-btn");
+const addBtns = document.querySelectorAll(".add-btn");
+
+const totalPrice = document.querySelector("#totalPrice");
+
+function handleNum(elemName, action) {
   let elem;
-  switch (size) {
-    case "lg":
-      elem = lgNum;
+  switch (elemName) {
+    case "yirgacheffeLgNum":
+      elem = yirgacheffeLgNum;
       break;
-    case "md":
-      elem = mdNum;
+    case "yirgacheffeMdNum":
+      elem = yirgacheffeMdNum;
       break;
-    case "sm":
-      elem = smNum;
+    case "yirgacheffeSmNum":
+      elem = yirgacheffeSmNum;
+      break;
+    case "costaricaLgNum":
+      elem = costaricaLgNum;
+      break;
+    case "costaricaMdNum":
+      elem = costaricaMdNum;
+      break;
+    case "costaricaSmNum":
+      elem = costaricaSmNum;
       break;
   }
   const num = Number(elem.innerHTML);
@@ -21,4 +39,34 @@ function handleNum(size, action) {
   } else if (num > 0) {
     elem.innerHTML = num - 1;
   }
+  calcPrice();
 }
+
+function calcPrice() {
+  const total =
+    (Number(yirgacheffeLgNum.innerHTML) + Number(costaricaLgNum.innerHTML)) *
+      600 +
+    (Number(yirgacheffeMdNum.innerHTML) + Number(costaricaMdNum.innerHTML)) *
+      360 +
+    (Number(yirgacheffeSmNum.innerHTML) + Number(costaricaSmNum.innerHTML)) *
+      150;
+  totalPrice.innerHTML = total;
+}
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2.5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1.5,
+      spaceBetween: 30,
+    },
+    680: {
+      slidesPerView: 2.5,
+    },
+  },
+});
